d1cf031107b5ecfcf31f64e6285a55d5d1ade10d
euguroglu <e.uguroglu@yahoo.com>
2021-05-23 18:47:23 +0300

    schema definition scala spark

--- SparkScalaParquet/src/main/scala/enesuguroglu/examples/Schema2.scala
+++ SparkScalaParquet/src/main/scala/enesuguroglu/examples/Schema2.scala
@@ -2,8 +2,9 @@ package enesuguroglu.examples
 
 import org.apache.log4j.Logger
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.types.{DateType, IntegerType, StringType, StructField, StructType}
 
-object Schema extends Serializable {
+object Schema2 extends Serializable {
 
   @transient lazy val logger: Logger = Logger.getLogger(getClass.getName)
   def main(args: Array[String]): Unit = {
@@ -14,12 +15,35 @@ object Schema extends Serializable {
       .master("local[3]")
       .getOrCreate()
 
+    //Defining schema
+    val flightSchemaStruct = StructType(List(
+      StructField("FL_DATE", DateType),
+      StructField("OP_CARRIER", StringType),
+      StructField("OP_CARRIER_FL_NUM", IntegerType),
+      StructField("ORIGIN", StringType),
+      StructField("ORIGIN_CITY_NAME", StringType),
+      StructField("DEST", StringType),
+      StructField("DEST_CITY_NAME", StringType),
+      StructField("CRS_DEP_TIME", IntegerType),
+      StructField("DEP_TIME", IntegerType),
+      StructField("WHEELS_ON", IntegerType),
+      StructField("TAXI_IN", IntegerType),
+      StructField("CRS_ARR_TIME", IntegerType),
+      StructField("ARR_TIME", IntegerType),
+      StructField("CANCELLED", IntegerType),
+      StructField("DISTANCE", IntegerType)
+    ))
+
+
     //Reading csv file
     val flightTimeCsvDf = spark.read
       .format("csv")
       .option("header", "true")
       .option("path", "data/flight*.csv")
-      .option("inferSchema", "true")
+      .option("mode", "FAILFAST")
+      .option("dateFormat", "M/d/y")
+    //  .option("inferSchema", "true")
+      .schema(flightSchemaStruct)
       .load()
 
     flightTimeCsvDf.show(5)
